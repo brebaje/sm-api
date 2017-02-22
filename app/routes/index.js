93972e67b573ea60d2d962607abfcae0239bdcf6
Manuel Bravo <manuelbravo@gmail.com>
2017-02-22 14:07:35 +0100

    Adding basic routes for healthcheck and docs

---
+++ app/routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const logger = require('../utils/log');
+
+const router = express.Router();
+
+router.get('/healthcheck', (req, res) => {
+  res.status(200).end();
+});
+
+router.get('/docs', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/docs.html'), null, (err) => {
+    if (err) {
+      logger.log(err);
+      res.status(err.status).end();
+    }
+  });
+});
+
+module.exports = router;
